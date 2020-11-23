3114a55fd55a9128b7d88655ea662886e56c31ed
choirul84 <aristhu_oracle@yahoo.com>
2020-11-23 07:37:58 +0700

    call api with async await

--- index.js
+++ index.js
@@ -82,3 +82,109 @@ console.log(nama, 'ada di', dusun)
 
 const [istriku, anakku] = cpFamily
 console.log(istriku, 'dan', anakku, 'forever')
+
+//class example
+class Car {
+    constructor(brand) {
+        this.brand = brand
+    }
+
+    // method biasa
+    present = () => {
+        return `Mobil saya adalah ${this.brand}`
+    }
+
+    // static method
+    static mine = (brand) => {
+        return `Mobil saya adalah ${brand}`
+    }
+}
+myCar = new Car('avanza')
+console.log(myCar.present())
+console.log(Car.mine('HRV'), '(static)')
+
+// inheritance sample
+class CarSport extends Car {
+    gardan = () => {
+        return `Mobil ${this.brand} punya dobel gardan (4x4)`
+    }
+}
+mySuv = new CarSport('Pajero')
+console.log(mySuv.present(), '(inheritance)')
+console.log(mySuv.gardan(), '(inheritance method)')
+
+// callback function (pengolahan data asynchronous dengan waktu delay)
+const callData = callback => {
+    setTimeout(() => {
+        const data = 'choirul'
+        callback(data)
+    }, 1000)
+}
+callData(name => console.log(`nama saya adalah ${name}`, '(callback)'))
+
+//chaining callback
+const callData2 = (firstname, callback) => {
+    setTimeout(() => {
+        const data = `${firstname} ihwan`
+        callback(data)
+    }, 1000)
+}
+callData(name => callData2(name, result => console.log(`nama saya adalah ${result}`, '(callback chaining)')))
+
+// promise (pengolahan data asynchronous dengan kemampuan chaining)
+const firstName = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const data = 'choirul'
+            resolve(data)
+        }, 1000)
+    })
+}
+
+const lastName = (name) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const data = `${name} ihwan`
+            resolve(data)
+        }, 1000)
+    })
+}
+
+firstName().then(data => lastName(data)).then(result => console.log(result, '(Promise)'))
+
+// async await (pengolahan data asynchronous dengan membuatnya sbg async)
+const firstName1 = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const data = 'choirul'
+            resolve(data)
+        }, 1000)
+    })
+}
+
+const lastName1 = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const data = `ihwan`
+            resolve(data) 
+        }, 1000)
+    })
+}
+
+const resultName = async() => {
+    const first = await firstName1()
+    const last = await lastName1()
+    return `${first} ${last}`
+}
+
+resultName().then(result => console.log(result, '(async await)'))
+
+// async and fetch
+const fetch = require('node-fetch')
+
+const callApi = async() => {
+    const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    return data.json()
+}
+
+callApi().then(result => console.log(result))
